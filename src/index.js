4c100ad0aafe9edd29563686eb5ea687afcc926c
srdjansolajic <solajic.srdjan@gmail.com>
2020-08-31 11:45:50 +0200

    kraj 3. testa

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import { Info } from './components/Info';
 import { Ships, selectBox } from './components/Ships';
-// import { getShips } from './service';
-
 
 const app = document.querySelector('#app');
-
-
-// getShips().then(res => {
-//     res.data.forEach(el => {
-//         if (el.active == true) {
-//             Ships()
-//         }
-//     });
-// })
-
 
 app.append(Info(), selectBox, Ships())
